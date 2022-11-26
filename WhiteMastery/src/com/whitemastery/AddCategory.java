f1aa132264c65a5b3c4cc1e0996dd28077f35bab
white <mohamedlahir1234@gmail.com>
2022-11-26 17:09:25 +0530

    ////

--- WhiteMastery/src/com/whitemastery/AddCategory.java
+++ WhiteMastery/src/com/whitemastery/AddCategory.java
@@ -36,7 +36,7 @@ public class AddCategory {
 		}
 
 		System.out.println("Number of Ranks :" + categoryRanks.size());
-
+//
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\white\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://pos.dinamic.io");
